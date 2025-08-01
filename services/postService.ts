81a094e2a3dbb76124add18a5a8441d951e00183
giatrieuu <trieulgse160392@fpt.edu.vn>
2025-08-02 02:42:18 +0700

    update

---
+++ services/postService.ts
@@ -0,0 +1,30 @@
+import { CreatePostRequest, CreatePostResponse, PostItem, UpdatePostRequest } from "@/types/post";
+import axiosInstance from "./axiosInstance";
+
+
+export const PostService = {
+  async createPost(data: CreatePostRequest): Promise<CreatePostResponse> {
+    const response = await axiosInstance.post("/api/Post", data);
+    return response.data;
+  },
+
+  async getPosts(): Promise<PostItem[]> {
+    const response = await axiosInstance.get("/api/Post");
+    return response.data.data;
+  },
+
+  async getPostById(id: string): Promise<PostItem> {
+    const response = await axiosInstance.get(`/api/Post/${id}`);
+    return response.data.data;
+  },
+  
+  async updatePost(data: UpdatePostRequest): Promise<{ success: boolean }> {
+    const response = await axiosInstance.put("/api/Post", data);
+    return response.data;
+  },
+
+  async deletePost(id: string): Promise<{ success: boolean }> {
+    const response = await axiosInstance.delete(`/api/Post/${id}`);
+    return response.data;
+  },
+};
