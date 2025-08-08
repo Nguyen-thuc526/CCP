b19af6792669dfb20345cd7653af487e7cb89ab3
giatrieuu <trieulgse160392@fpt.edu.vn>
2025-08-08 13:02:53 +0700

    up

--- app/admin/managecourse/[id]/page.tsx
+++ app/admin/managecourse/[id]/page.tsx
@@ -7,14 +7,6 @@ interface CourseDetailPageProps {
 }
 
 export default async function AdminCourseDetailPage({ params }: CourseDetailPageProps) {
-  // Nếu params là Promise, hãy await:
-  // const resolvedParams = await params;
-  // return (
-  //   <div className="space-y-6">
-  //     <AdminCourseDetail courseId={resolvedParams.id} />
-  //   </div>
-  // )
-  // Nhưng thông thường, Next.js truyền params trực tiếp, chỉ cần async function là đủ:
   return (
     <div className="space-y-6">
       <AdminCourseDetail courseId={params.id} />
